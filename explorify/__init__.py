77b1b72f0b020c958268ae4c578bb8caafe52758
Tom Fleischer <tfleischer@gmail.com>
2013-07-29 03:08:19 +0000

    Added call to API

--- explorify/__init__.py
+++ explorify/__init__.py
@@ -1,11 +1,28 @@
+import csv
+import requests
+import StringIO
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello()
-  return 'Hello World!'
+@app.route("/:album_id")
+def get_track_data(album_id):
+  resp = requests.get(
+    "http:/ws.spotify.com/lookup/1/.json?uri=spotify:album:%s" % album_id)
+  tracks = resp.json()['album']['tracks']
+  track_data = [(t['name'], t['track-number'], t['popularity']) \
+                for t in tracks]
 
+  fp = StringIO.StringIO()
+  writer = csv.writer(fp)
+  track_data = sorted(track_data, key=lambda t: t[1])
+  for track in track_data:
+    name, num, pop = track
+    writer.writerow([name, num, pop])
 
-if __name__ == "__main__"
+  writer.flush()
+  return fp.getvalue()
+
+if __name__ == "__main__":
   app.run()
